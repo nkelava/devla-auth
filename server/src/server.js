b586b8c4a248c9ea1c3c7ee2ed09741d99d6149e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-09 16:32:30 +0100

    Add env port to server entry point script

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
 });
