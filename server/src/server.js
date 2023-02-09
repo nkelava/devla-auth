f408f85b0c37758cf28961b73ac295350c131bfa
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-09 19:04:58 +0100

    Update server entry point port with env var

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
