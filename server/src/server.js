f8f42f9f735a63d53b3fc577182cdbec5aee499b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 22:01:59 +0100

    Add auth router middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./user");
-const { DB_USERNAME, DB_PASSWORD, DB_IP, DB_PORT, DB_NAME } = require("./config/db.config");
+const { authRouter } = require("./auth");
+const { DB_USERNAME, DB_PASSWORD, DB_IP, DB_PORT, DB_NAME } = require("./db/db.config");
 
-const port = process.env.SERVER_PORT || 3000;
 const mongoURL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_IP}:${DB_PORT}/${DB_NAME}`;
 
 mongoose
@@ -11,18 +11,17 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Successfully connected to database..."))
-  .catch((error) => console.log(error));
+  .then(() => console.log("Successfully connected to the database..."))
+  .catch((err) => console.error(err));
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
-app.get("/", (_req, resp) => {
-  resp.send("<h1>Hello Universe!</h1>");
-});
+const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
