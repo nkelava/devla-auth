2c09e414d7fa362a8c226797b089f1fef8dd1921
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 01:02:38 +0100

    Add db connection test

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { DB_USERNAME, DB_PASSWORD, DB_IP, DB_PORT, DB_NAME } = require("./config/config");
+
+const mongoURL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_IP}:${DB_PORT}/${DB_NAME}`;
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Successfully connected to database..."))
+  .catch((error) => console.log(error));
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.get("/", (_req, resp) => {
+  resp.send("<h1>Hello Universe!!!</h1>");
+});
 
 const port = process.env.SERVER_PORT || 3000;
 
