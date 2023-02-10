f694cc581e161207cf0455160a272a316c56e1a1
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 01:48:25 +0100

    Update user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -4,10 +4,12 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     require: [true, "User must have email."],
+    unique: true,
   },
   password: {
     type: String,
     require: [true, "User must have password."],
+    minLength: [8, "Password is too short."],
   },
 });
 
