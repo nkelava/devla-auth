967f2dd0e0c875d29c13c60be47dfc04457f9b20
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 12:47:07 +0100

    Rename db config file and some related env variables

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DB_USERNAME: process.env.MONGO_INITDB_ROOT_USERNAME,
   DB_PASSWORD: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  DB_IP: process.env.DB_IP || "mongo",
-  DB_PORT: process.env.DB_PORT || 27017,
+  DB_IP: process.env.MONGO_INITDB_IP,
+  DB_PORT: process.env.MONGO_INITDB_PORT || 27017,
   DB_NAME: process.env.MONGO_INITDB_DATABASE,
 };
