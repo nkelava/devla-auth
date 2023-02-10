b940e2282081a7d3f168f0d7309b4cd81087b2d8
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 20:12:30 +0100

    Rename and reorganize files and env variables considering db

--- server/src/user/user.model.js
+++ server/src/user/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     require: [true, "User must have email."],
@@ -13,6 +13,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
