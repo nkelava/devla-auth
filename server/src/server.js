129ba1de0d5c32dafb112a15f63c27eebd9ab3ca
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 02:34:20 +0100

    Add user router

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/user.route");
 const { DB_USERNAME, DB_PASSWORD, DB_IP, DB_PORT, DB_NAME } = require("./config/config");
 
+const port = process.env.SERVER_PORT || 3000;
 const mongoURL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_IP}:${DB_PORT}/${DB_NAME}`;
 
 mongoose
@@ -16,12 +18,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/api/v1/users", userRouter);
 
 app.get("/", (_req, resp) => {
   resp.send("<h1>Hello Universe!!!</h1>");
 });
-
-const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
