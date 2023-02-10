a980aebc3e2db1c7569b6fc0a29bf0201ef6367b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 23:27:49 +0100

    Add login action method

--- server/src/auth/auth.controller.js
+++ server/src/auth/auth.controller.js
@@ -21,3 +21,32 @@ exports.register = async (req, resp) => {
     });
   }
 };
+
+exports.login = async (req, resp) => {
+  const { email, password } = req.body;
+
+  try {
+    const existedUser = await User.findOne({
+      email,
+      password,
+    });
+
+    if (!existedUser) {
+      resp.status(404).json({
+        status: "fail",
+        message: "There is no user with that email and password.",
+      });
+    }
+
+    resp.status(201).json({
+      status: "success",
+      data: {
+        user: existedUser,
+      },
+    });
+  } catch (err) {
+    resp.status(400).json({
+      status: "fail",
+    });
+  }
+};
