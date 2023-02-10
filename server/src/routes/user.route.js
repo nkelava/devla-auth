1a183c164204eefe90a1feff9a93692f3884bed7
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-10 01:54:35 +0100

    Add user router

---
+++ server/src/routes/user.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const userController = require("../controllers/user.controller");
+
+const router = express.Router();
+
+router.route("/").post(userController.createUser);
+router.route("/:id").get(userController.getUserById).delete(userController.deleteUserById);
