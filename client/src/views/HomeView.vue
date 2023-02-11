4b9a0afe8da79023f77c653acf5bf0d203769d78
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 14:36:04 +0100

    Add simple user display at home page also no validation, etc

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,17 @@
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const user = ref();
+onMounted(async () => {
+  const data = await axios.get(`http://localhost:3000/api/v1/user/63e79729260e25cdab7d9d1b`);
+  user.value = data.data.data.user;
+});
+</script>
+s
+
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>{{ user?.email }}</h1>
   </div>
 </template>
