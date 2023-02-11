fe174bc6c3c80408703bf3f5b212d116e4cde6e7
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 23:43:46 +0100

    Add status route

--- server/src/auth/auth.router.js
+++ server/src/auth/auth.router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authController = require("./auth.controller");
+const { validateToken } = require("./auth.middleware");
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/status", validateToken, authController.status);
 
 module.exports = router;
