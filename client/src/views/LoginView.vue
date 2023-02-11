31b5a9387f934e68f9c780a3838f3006c5c42320
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 14:35:05 +0100

    Add simple login without extra validation

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,5 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const form = ref({
+  email: "",
+  password: "",
+});
+
+const handleLogin = async () => {
+  await axios.post(`http://localhost:3000/api/v1/auth/login`, {
+    email: form.value.email,
+    password: form.value.password,
+  });
+
+  router.push("/");
+};
+</script>
+
 <template>
   <div>
-    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <input v-model="form.email" type="email" name="email" placeholder="Enter email..." />
+      <input v-model="form.password" type="password" name="password" placeholder="Enter password..." />
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
