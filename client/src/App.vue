c3d39bbea96395745ba3736b0779c06ca67a7e19
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 15:29:04 +0100

    Add nav links for easier development

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <RouterLink to="/" class="nav-item">Home</RouterLink>
+  <RouterLink to="/login" class="nav-item">Login</RouterLink>
+  <RouterLink to="/register" class="nav-item">Register</RouterLink>
+
   <RouterView />
 </template>
+
+<style>
+.nav-item {
+  margin: 10px;
+}
+</style>
