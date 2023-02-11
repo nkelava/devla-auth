608c9e144a1b4833b13a274979beec0f413e864e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 14:43:42 +0100

    Add simple register page logic - testing purposes

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,5 +1,40 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const form = ref({
+  email: "",
+  password: "",
+  repeatedPassword: "",
+});
+
+const handleRegister = async () => {
+  if (form.value.password !== form.value.repeatedPassword) {
+    form.value.error = "Passwords doesn not match.";
+    return;
+  }
+
+  await axios.post(`http://localhost:3000/api/v1/auth/register`, {
+    email: form.value.email,
+    password: form.value.password,
+  });
+
+  router.push("/login");
+};
+</script>
+
 <template>
   <div>
-    <h1>Register</h1>
+    <form @submit.prevent="handleRegister">
+      <div v-if="form.error" class="form-error">
+        {{ form.error }}
+      </div>
+      <input v-model="form.email" type="email" name="email" placeholder="Enter email..." />
+      <input v-model="form.password" type="password" name="password" placeholder="Enter password..." />
+      <input v-model="form.repeatedPassword" type="password" name="password" placeholder="Repeat password..." />
+      <button type="submit">Register</button>
+    </form>
   </div>
 </template>
