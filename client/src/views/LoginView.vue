adabd57aac82a37355c42ae7d7d4dc27a5d316e4
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-11 20:58:57 +0100

    Add access tocker check for testing purpose

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -4,16 +4,19 @@ import axios from "axios";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
 const form = ref({
   email: "",
   password: "",
 });
 
 const handleLogin = async () => {
-  await axios.post(`http://localhost:3000/api/v1/auth/login`, {
-    email: form.value.email,
-    password: form.value.password,
-  });
+  await axios
+    .post(`http://localhost:3000/api/v1/auth/login`, {
+      email: form.value.email,
+      password: form.value.password,
+    })
+    .then((response) => console.log(response.data.accessToken));
 
   router.push("/");
 };
