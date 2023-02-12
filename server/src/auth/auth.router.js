1cc0f2b0b158edfa356bda734c44e9b1da963807
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-12 01:46:04 +0100

    Add logout router

--- server/src/auth/auth.router.js
+++ server/src/auth/auth.router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 router.get("/status", validateToken, authController.status);
 
 module.exports = router;
