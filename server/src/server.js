96397cbe02531d14a218e31611b8523f5458aca5
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-12 01:43:38 +0100

    Add jwt route

--- server/src/server.js
+++ server/src/server.js
@@ -22,6 +22,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/v1/jwt");
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
 
