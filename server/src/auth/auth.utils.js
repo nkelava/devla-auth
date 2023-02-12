90312cdc879ddb9475337676a258c32cfcfc0f4b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-12 01:30:14 +0100

    Remove email from token

--- server/src/auth/auth.utils.js
+++ server/src/auth/auth.utils.js
@@ -3,7 +3,6 @@ const { sign } = require("jsonwebtoken");
 const generateToken = (user, secret, expiresIn) => {
   const userDTO = {
     id: user.id,
-    email: user.email,
   };
 
   return sign(userDTO, secret, { expiresIn });
