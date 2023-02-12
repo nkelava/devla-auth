4dd94b52cbc8d3915553a0cd6b1dff2fe80298d9
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-12 18:14:42 +0100

    Add nav links to home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { onMounted } from "vue";
 import { useUserStore } from "../stores/user.store";
+import { RouterLink } from "vue-router";
 
 const store = useUserStore();
 
@@ -13,7 +14,15 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <h1>{{ store.user?.email }}</h1>
-  </div>
+  <RouterLink :to="{ name: 'home' }" class="nav-item">Home</RouterLink>
+  <RouterLink :to="{ name: 'login' }" class="nav-item">Login</RouterLink>
+  <RouterLink :to="{ name: 'register' }" class="nav-item">Register</RouterLink>
+
+  <h1>{{ store.user?.email }}</h1>
 </template>
+
+<style>
+.nav-item {
+  margin: 20px;
+}
+</style>
