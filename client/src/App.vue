58f2f5f76ad2f793e5fed2f3c9aa213ef93c3049
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-12 18:14:15 +0100

    Move nav links to home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'home' }" class="nav-item">Home</RouterLink>
-  <RouterLink :to="{ name: 'login' }" class="nav-item">Login</RouterLink>
-  <RouterLink :to="{ name: 'register' }" class="nav-item">Register</RouterLink>
-
   <RouterView />
 </template>
-
-<style>
-.nav-item {
-  margin: 10px;
-}
-</style>
