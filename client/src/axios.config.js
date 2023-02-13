aa6ecf041673df34a2a5c5dfa6c4bde5395489a2
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-13 01:03:17 +0100

    Add default header and credentials options in axios config

--- client/src/axios.config.js
+++ client/src/axios.config.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const accessToken = JSON.parse(window.localStorage.getItem("accessToken"));
+const user = JSON.parse(window.localStorage.getItem("user"));
 
-if (accessToken) {
-  axios.defaults.headers.common["Authorization"] = "Bearer " + accessToken;
+if (user) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + user.accessToken;
 }
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+axios.defaults.withCredentials = true;
