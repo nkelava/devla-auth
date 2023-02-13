fd177a8ef90ad34870abb12044e4d9c534ca7bbc
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-13 20:22:37 +0100

    Wrap login and form inside container

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -18,24 +18,26 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <div class="da-form-container">
-    <h2>Login</h2>
+  <div class="da-container">
+    <div class="da-form-container">
+      <h2>Login</h2>
 
-    <form @submit.prevent="handleLogin">
-      <div class="da-input-wrapper">
-        <input v-model="form.email" type="email" name="email" required />
-        <span>Email</span>
-        <i></i>
-      </div>
-      <div class="da-input-wrapper">
-        <input v-model="form.password" type="password" name="password" required />
-        <span>Password</span>
-        <i></i>
-      </div>
-      <input type="submit" value="Login" />
-      <div class="da-link-wrapper">
-        <RouterLink class="link da-link" :to="{ name: 'register' }">Don't have an account? Sign up</RouterLink>
-      </div>
-    </form>
+      <form @submit.prevent="handleLogin">
+        <div class="da-input-wrapper">
+          <input v-model="form.email" type="email" name="email" required />
+          <span>Email</span>
+          <i></i>
+        </div>
+        <div class="da-input-wrapper">
+          <input v-model="form.password" type="password" name="password" required />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <input type="submit" value="Login" />
+        <div class="da-link-wrapper">
+          <RouterLink class="link da-link" :to="{ name: 'register' }">Don't have an account? Sign up</RouterLink>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -26,33 +26,35 @@ const handleRegister = async () => {
 </script>
 
 <template>
-  <div class="da-form-container">
-    <div v-if="registerStore.error">
-      {{ registerStore.error }}
+  <div class="da-container">
+    <div class="da-form-container">
+      <div v-if="registerStore.error">
+        {{ registerStore.error }}
+      </div>
+
+      <h2>Sign Up</h2>
+
+      <form @submit.prevent="handleRegister">
+        <div class="da-input-wrapper">
+          <input v-model="registerStore.email" type="email" name="email" required />
+          <span>Email</span>
+          <i></i>
+        </div>
+        <div class="da-input-wrapper">
+          <input v-model="registerStore.password" type="password" name="password" required />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <div class="da-input-wrapper">
+          <input v-model="registerStore.repeatedPassword" type="password" name="repeated" required />
+          <span>Repeat password</span>
+          <i></i>
+        </div>
+        <input type="submit" value="Sign Up" />
+        <div class="da-link-wrapper">
+          <RouterLink class="link da-link" :to="{ name: 'login' }">Already have an account? Login</RouterLink>
+        </div>
+      </form>
     </div>
-
-    <h2>Sign Up</h2>
-
-    <form @submit.prevent="handleRegister">
-      <div class="da-input-wrapper">
-        <input v-model="registerStore.email" type="email" name="email" required />
-        <span>Email</span>
-        <i></i>
-      </div>
-      <div class="da-input-wrapper">
-        <input v-model="registerStore.password" type="password" name="password" required />
-        <span>Password</span>
-        <i></i>
-      </div>
-      <div class="da-input-wrapper">
-        <input v-model="registerStore.repeatedPassword" type="password" name="repeated" required />
-        <span>Repeat password</span>
-        <i></i>
-      </div>
-      <input type="submit" value="Sign Up" />
-      <div class="da-link-wrapper">
-        <RouterLink class="link da-link" :to="{ name: 'login' }">Already have an account? Login</RouterLink>
-      </div>
-    </form>
   </div>
 </template>
