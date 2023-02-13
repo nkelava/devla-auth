163684b61e0679f26b16ec3c2412c4e4fa3e72dc
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-13 01:14:50 +0100

    Add status link for testing purposes

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,12 +2,31 @@
 import { onMounted } from "vue";
 import { useUserStore } from "../stores/user.store";
 import { RouterLink } from "vue-router";
+import axios from "axios";
 
 const userStore = useUserStore();
 
 onMounted(async () => {
   await userStore.getUser();
 });
+
+const handleStatus = async () => {
+  try {
+    const data = await axios.get("api/v1/auth/status");
+
+    if (data.data.user) {
+      console.log("User: ", data.data.user);
+      return;
+    }
+
+    const accessToken = data.data.accessToken;
+    console.log("New access token: ", accessToken);
+
+    if (accessToken) await userStore.updateAccessToken(accessToken);
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <template>
@@ -19,10 +38,16 @@ onMounted(async () => {
     <RouterLink :to="{ name: 'login' }" class="nav-item">Login</RouterLink>
     <RouterLink :to="{ name: 'register' }" class="nav-item">Register</RouterLink>
   </div>
+  <button :onclick="handleStatus">Status</button>
 </template>
 
 <style setup>
-.nav-item {
+.nav-item,
+h1 {
+  margin-left: 30px;
+}
+
+button {
   margin: 30px;
 }
 </style>
