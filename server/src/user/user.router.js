54980c23ba8b833feab4d3b897c98d54e8ea367f
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 08:51:55 +0100

    Add register middleware before register route

--- server/src/user/user.router.js
+++ server/src/user/user.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userController = require("./user.controller");
+const { validateRegister } = require("./user.middleware");
 
 const router = express.Router();
 
-router.route("/").post(userController.createUser);
+router.post("/", validateRegister, userController.createUser);
 router.route("/:id").get(userController.getUserById).delete(userController.deleteUserById);
 
 module.exports = router;
