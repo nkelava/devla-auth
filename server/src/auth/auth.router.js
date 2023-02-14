b910b4e01b228395990eb5a5608a5b74a3b27ab9
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 08:53:10 +0100

    Implement login validator

--- server/src/auth/auth.router.js
+++ server/src/auth/auth.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authController = require("./auth.controller");
 const { validateToken } = require("../jwt/jwt.middleware");
+const { validateLogin } = require("./auth.middleware");
 
 const router = express.Router();
 
-router.post("/login", authController.login);
+router.post("/login", validateLogin, authController.login);
 router.post("/logout", authController.logout);
 router.get("/status", validateToken, authController.status);
 
