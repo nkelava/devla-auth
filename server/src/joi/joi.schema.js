bdbfc753486725bd4da87625eacb3c16b28dd52e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 09:38:36 +0100

    Add custom messages to joi schemas

--- server/src/joi/joi.schema.js
+++ server/src/joi/joi.schema.js
@@ -1,13 +1,25 @@
 const joi = require("joi");
 
 const loginSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().required().min(8),
+  email: joi.string().email().required().messages({
+    "string.email": "Wrong email format e.g. example@gmail.com. 😊",
+    "string.empty": "Email cannot be an empty field. 😊",
+  }),
+  password: joi.string().required().min(8).messages({
+    "string.min": "Password should be at least 8 characters long. 😊",
+    "string.empty": "Password cannot be an empty field. 😊",
+  }),
 });
 
 const registerSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().required().min(8),
+  email: joi.string().email().required().messages({
+    "string.email": "Wrong email format e.g. example@gmail.com. 😊",
+    "string.empty": "Email cannot be an empty field. 😊",
+  }),
+  password: joi.string().required().min(8).messages({
+    "string.min": "Password should be at least 8 characters long. 😊",
+    "string.empty": "Password cannot be an empty field. 😊",
+  }),
 });
 
 module.exports = {
