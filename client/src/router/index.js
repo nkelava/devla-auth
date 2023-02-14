e1f9fa42e9741c73a62485ba967f2d1b1f3c787b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 12:49:00 +0100

    Remove unused code in client router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,14 +2,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useUserStore } from "../stores/user.store";
 
-const checkAuth = () => {
-  // call await axios("api/v1/auth/status");
-  // check if user is auth
-  // if true return true
-  // if not ask for new access token
-  // if goes well return true, otherwise return false
-};
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,10 +11,8 @@ const router = createRouter({
       name: "home",
       component: () => import("../views/HomeView.vue"),
       beforeEnter: (to, from, next) => {
-        // TODO: first check if its logged in than if its authorizes
-        // eg. beforeEnter : guardMyroute,
-        // eg. beforeEnter: [removeQueryParams, removeHash],
-        [useUserStore().user.id ? next() : next({ name: "login" }), checkAuth];
+        // TODO: add auth check
+        [useUserStore().user.id ? next() : next({ name: "login" })];
       },
     },
     {
