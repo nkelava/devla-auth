b2bc43109040d993fe252b9914debe50d8d87148
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 04:19:27 +0100

    Update responses in user controller actions

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -3,9 +3,12 @@ const User = require("./user.model");
 exports.getUserById = async (req, resp) => {
   try {
     const { id } = req.params;
+
+    if (!id) return resp.status(400).json({ error: "Bad request." });
+
     const user = await User.findOne({ _id: id });
 
-    if (!user) resp.status(404).json({ error: "Account does not exist." });
+    if (!user) return resp.status(404).json({ error: "Account does not exist." });
 
     const userDTO = {
       id: user.id,
@@ -22,7 +25,7 @@ exports.createUser = async (req, resp) => {
   try {
     const { email, password } = req.body;
 
-    if (!email || !password) return resp.status(400).json({ error: "Please make sure to enter all the data." });
+    if (!email || !password) return resp.status(400).json({ error: "Bad request." });
 
     const newUser = await User.create({
       email,
@@ -31,16 +34,19 @@ exports.createUser = async (req, resp) => {
 
     resp.status(201).json({ user: newUser });
   } catch (err) {
-    resp.status(400).json({ error: "Oops. Please, try again with another email." });
+    resp.status(400).json({ error: "Bad request." });
   }
 };
 
 exports.deleteUserById = async (req, resp) => {
   try {
     const { id } = req.params;
+
+    if (!id) return resp.status(400).json({ error: "Bad request." });
+
     const user = await User.findByIdAndDelete({ _id: id });
 
-    if (!user) return resp.sendStatus(400);
+    if (!user) return resp.status(400).json({ error: "Bad request." });
 
     resp.sendStatus(204);
   } catch (err) {
