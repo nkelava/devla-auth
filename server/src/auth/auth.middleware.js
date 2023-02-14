aa75f6dfa180f65bd44491894f32f8f3f49d3367
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-14 09:39:11 +0100

    Add one message in case of error from validators, for now

--- server/src/auth/auth.middleware.js
+++ server/src/auth/auth.middleware.js
@@ -3,9 +3,7 @@ const { loginValidator } = require("../joi/joi.validator");
 const validateLogin = (req, resp, next) => {
   const { error } = loginValidator(req.body);
 
-  if (error) {
-    return resp.status(400).json({ error: "Make sure you enter data correctly 😊" });
-  }
+  if (error) return resp.status(400).json({ error: error.details[0].message });
 
   next();
 };
--- server/src/user/user.middleware.js
+++ server/src/user/user.middleware.js
@@ -3,9 +3,7 @@ const { registerValidator } = require("../joi/joi.validator");
 const validateRegister = (req, resp, next) => {
   const { error } = registerValidator(req.body);
 
-  if (error) {
-    return resp.json({ error: error.details });
-  }
+  if (error) return resp.status(400).json({ error: error.details[0].message });
 
   next();
 };
