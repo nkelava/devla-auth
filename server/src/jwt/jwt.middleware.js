1adfd8676ceb9a8ffc56e05b5af4d4057a2c3d8e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-16 00:43:12 +0100

    Update response error messages

--- server/src/jwt/jwt.middleware.js
+++ server/src/jwt/jwt.middleware.js
@@ -5,13 +5,12 @@ const errorMessages = require("../error/error.consts");
 const validateToken = (req, resp, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
-  if (!authHeader?.startsWith("Bearer "))
-    return resp.status(401).json({ error: errorMessages.ACCOUNT_NOT_AUTHENTICATED });
+  if (!authHeader?.startsWith("Bearer ")) return resp.status(401).json({ error: errorMessages.ACCOUNT_UNAUTHORIZED });
 
   const accessToken = authHeader.split(" ")[1];
 
   verify(accessToken, jwtConfig.access_token.secret, (err, decoded) => {
-    if (err || !decoded.id) return resp.status(403).json({ error: "Invalid token." });
+    if (err || !decoded.id) return resp.status(403).json({ error: errorMessages.ACCOUNT_FORBIDDEN });
 
     req.id = decoded.id;
     next();
